63fce7ca43b7420a5c4525c0b08c4e62e302b75f
Arki 3 <fvortexxxq@gmail.com>
2025-12-31 19:22:45 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,2 +1,44 @@
-Echo Installing!
-npm install
+@echo off
+title Installer
+
+echo Checking for Node.js...
+where node >nul 2>nul
+
+if %errorlevel% neq 0 (
+    echo.
+    echo Node.js is NOT installed.
+    echo Please install Node.js from https://nodejs.org, run the installer - MAKE SURE YOU "ADD TO PATH"
+    echo Then reopen this installer.
+    echo.
+    pause
+    exit /b
+)
+
+echo ok found node
+node -v
+echo.
+
+echo Installing packages...
+npm i debug
+if %errorlevel% neq 0 goto npmerror
+
+npm i discord.js-selfbot-v13
+if %errorlevel% neq 0 goto npmerror
+
+npm i open
+if %errorlevel% neq 0 goto npmerror
+
+npm i chalk
+if %errorlevel% neq 0 goto npmerror
+
+echo.
+echo Done!
+pause
+exit /b
+
+:npmerror
+echo.
+echo An error occurred during npm install.
+echo Make sure you have internet access and permissions.
+pause
+exit /b
